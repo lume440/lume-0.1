69439269a4813637aa13cc3f441958ebf1a1e5b3
lume440 <68623718+lume440@users.noreply.github.com>
2026-02-25 11:26:14 -0800

    Create basic.js
    First on screen visualization dev

---
+++ lindenhurst /visualizations /basic.js
@@ -0,0 +1,23 @@
+var mic, fft;
+
+function setup() {
+   createCanvas(320,240);
+   noFill();
+
+   mic = new p5.AudioIn();
+   mic.start();
+   fft = new p5.FFT();
+   fft.setInput(mic);
+}
+
+function draw() {
+   background(244, 65, 92)
+
+   var spectrum = fft.analyze();
+
+   beginShape();
+   for (i = 0; i<spectrum.length; i++) {
+    vertex(i, map(spectrum[i], 0, 255, height, 0) );
+   }
+   endShape();
+}
