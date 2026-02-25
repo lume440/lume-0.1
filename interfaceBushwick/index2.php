1fb5be9d841005d6ed1d4ed97aaa5fc5b366efd8
lume440 <68623718+lume440@users.noreply.github.com>
2026-02-25 11:04:15 -0800

    Add files via upload

---
+++ interfaceBushwick/index2.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Semantic UI CDN</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.css"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.js"></script>
+<style type="text/css">
+body {
+    overflow-x:hidden;
+}
+::-webkit-scrollbar { 
+    display: none; 
+}
+</style>
+<html>
+<body>
+
+<div class="container">
+<div class="ui raised card">
+  <div class="content">
+    <div class="header">Tuner <i class="right floated red music icon"></i> </div>
+    <div class="meta">
+      <span class="category">Tune your shit, yo</span>
+    </div>
+    <div class="description">
+      <p></p>
+    </div>
+  </div>
+  <div class="extra content">
+  <button class="ui green inverted button" onclick="window.location.href='tunerList.php'">Start</button>
+   <button class="ui blue inverted button"><a href="tunerSettings.php">Settings</button>
+    </div>
+  </div>
+</div>
+<div class="ui divider"></div>
+<div class="ui raised card">
+  <div class="content">
+    <div class="header">Practice<i class="right floated blue student icon"></i></div>
+    <div class="meta">
+      <span class="category">Get your learn on.</span>
+    </div>
+    <div class="description">
+      <p></p>
+    </div>
+  </div>
+  <div class="extra content">
+  <button class="ui green inverted button">Start</button>
+   <button class="ui blue inverted button">Settings</button>
+    </div>
+  </div>
+</div>
+
+<div class="ui raised card">
+  <div class="content">
+    <div class="header">Profile<i class="right floated purple setting icon"></i></div>
+    <div class="meta">
+      <span class="category">Set up your instrument and other settings</span>
+    </div>
+    <div class="description">
+      <p></p>
+    </div>
+  </div>
+  <div class="extra content">
+ <button class="ui green inverted button">Start</button>
+   <button class="ui blue inverted button">Import</button>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>
+<script type="text/javascript">
+
+</script>
+
